0268e3ee7ad0476dce25da0ea411e15fb0de964b
Ahokas Maria Katariina <maria.ahokas@stud.pointcollege.fi>
2019-03-21 20:17:32 +0200

    Mobile Front End

--- MobileFront/MobileFront.Android/MainActivity.cs
+++ MobileFront/MobileFront.Android/MainActivity.cs
@@ -6,20 +6,64 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Locations;
 
 namespace MobileFront.Droid
 {
     [Activity(Label = "MobileFront", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity :
+        global::Xamarin.Forms.Platform.Android.FormsApplicationActivity,
+        ILocationListener
     {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            TabLayoutResource = Resource.Layout.Tabbar;
-            ToolbarResource = Resource.Layout.Toolbar;
+        public static Android.Locations.LocationManager LocationManager;
 
-            base.OnCreate(savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+
+            // käynnistetään GPS-paikannus
+            try
+            {
+                LocationManager = GetSystemService(
+                    "location") as LocationManager;
+                string Provider = LocationManager.GpsProvider;
+
+                if (LocationManager.IsProviderEnabled(Provider))
+                {
+                    LocationManager.RequestLocationUpdates(
+                        Provider, 2000, 1, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+            }
+        }
+
+        public void OnLocationChanged(Location location)
+        {
+            MobileFront.Models.GpsLocationModel.Latitude = location.Latitude;
+            MobileFront.Models.GpsLocationModel.Longitude = location.Longitude;
+            MobileFront.Models.GpsLocationModel.Altitude = location.Altitude;
+        }
+
+        public void OnProviderDisabled(string provider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnProviderEnabled(string provider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void OnStatusChanged(string provider, [GeneratedEnum] Availability status, Bundle extras)
+        {
+            //throw new NotImplementedException();
         }
     }
 }
+
---
+++ MobileFront/MobileFront/Models/GpsLocationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileFront.Models
+{
+    public class GpsLocationModel
+    {
+        //static tarkoittaa sitä että alla olevia tietoja on vaan yksi sovelluksessa
+        public static double Latitude { get; set; }
+        public static double Longitude { get; set; }
+        public static double Altitude { get; set; }
+    }
+}
